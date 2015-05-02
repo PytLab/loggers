00a3b8e75d0a063e48113eb6a5e1c4b8e7038983
pytlab <shaozhengjiang@gmail.com>
2015-05-02 15:55:58 +0800

    Use muilt-thread to download literature
    此脚本根据doi号依次检测各数据库，然后下载相应文献到此当前路径下，文件名问数据库.pdf,例如 ACS.pdf.

--- scripts/doi_logger/run_doi.py
+++ scripts/doi_logger/run_doi.py
@@ -1,6 +1,86 @@
 import sys
 sys.path.append('D:\Dropbox\Code\Python\loggers')
 from loggers.doi_logger import *
+import threading
 
-url = 'http://dx.doi.org/'
-m = DoiLogger(need_data_file=False)
+
+class GetPdfUrlThread(threading.Thread):
+    def __init__(self, func, args, name):
+        threading.Thread.__init__(self)
+        self.func = func
+        self.args = args
+        self.name = name
+
+    def get_result(self):
+        return self.url, self.name
+
+    def run(self):
+        print "\nCheck: %s...\n" % self.name
+        self.url = apply(self.func, self.args)
+        if self.url:
+            print "\nIn %s.\n" % self.name
+        else:
+            print "\nNot in %s.\n" % self.name
+
+
+if len(sys.argv) != 2:
+    print "Usage: %s doi number.\n" % (sys.argv[0])
+else:
+    doi_number = str(sys.argv[1])
+    print 'doi: %s\n' % doi_number
+    #instance of DoiLogger
+    print "Create logger instance..."
+    m = DoiLogger(need_data_file=False)
+    print 'Ok.\n'
+    #get page content
+    form_data_dict = {'hdl': doi_number}
+    print "Geting page content..."
+    print "This may take a minute, please wait..."
+    page_content = m.send_post(m.url_login, form_data_dict)
+    print "Ok.\n"
+
+    #gather thread objects
+    threads = []
+    for database_type in m.database_dict:
+        #method to get pdf url
+        func = m.database_dict[database_type]
+        #get args(tuple)
+        if database_type == 'ACS':
+            args = (page_content, doi_number)
+        elif database_type == 'Wiley':
+            args = (doi_number, )
+        else:
+            args = (page_content, )
+
+        #create thread object
+        t = GetPdfUrlThread(func, args, database_type)
+        threads.append(t)
+
+    nthreads = len(threads)  # number of thread
+
+    #start threads
+    for i in xrange(nthreads):
+        threads[i].start()
+
+    for i in xrange(nthreads):
+        threads[i].join()
+        url, name = threads[i].get_result()
+        if url:
+            pdf_url = url
+            database_name = name
+
+    #download pdf
+    #choose download method
+    if database_name == 'Springer':
+        download_method = m.download_pdf_by_urllib
+        print "Use urllib to download..."
+    else:
+        download_method = m.download_pdf_by_requests
+        print "Use requests to download..."
+
+    save_name = database_name + '.pdf'
+    target_path = './'
+
+    print "Downloading %s...\n" % save_name
+    download_method(pdf_url, target_path, save_name)
+    print 'Ok. Go and check your file!'
